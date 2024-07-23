e3ea4eefa9bfcdea2d96077aecc5ba88c59aa1a6
Noya50 <noyaleshem12@gmail.com>
2024-07-23 09:54:28 +0300

    (main.tf) add diagnostic setting

--- main.tf
+++ main.tf
@@ -8,3 +8,11 @@ resource "azurerm_virtual_network" "this" {
     ignore_changes = [tags]
   }
 }
+
+module "diagnostic_setting" {
+  source = "git::https://github.com/Noya50/hafifot-diagnosticSetting.git?ref=main"
+
+  name                       = "${azurerm_virtual_network.this.name}-diagnostic-setting"
+  target_resource_id         = azurerm_virtual_network.this.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+}
